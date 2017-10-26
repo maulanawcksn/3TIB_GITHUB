60a8c3ed7f561ab4ba4b8e1a387a47a895586e9f
Maulana Wicaksono <33107407+maulanawcksn@users.noreply.github.com>
2017-10-26 10:06:59 +0700

    Update salam.aspx.vb

--- Assalamualaikum/Coba/salam.aspx.vb
+++ Assalamualaikum/Coba/salam.aspx.vb
@@ -4,5 +4,9 @@
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
+    
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = "Nama anda " & txtnama.Text & "<br> NIM anda " & txtnim.Text & "<br> Prodi anda " & txtkelas
+    End Sub
 
 End Class
